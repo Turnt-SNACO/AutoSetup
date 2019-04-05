ee59d4015b0ef6d1c36243184fb1dd4bd465e4f1
Turnt-SNACO <janderson5@albany.edu>
2019-04-05 18:11:55 -0400

    Initial commit

---
+++ AutoSetup.ps1
@@ -0,0 +1,13 @@
+﻿echo "Temporarily adjusting the execution policy for installation..."
+Set-Variable -Name "execution_policy" -Value (Get-ExecutionPolicy)
+Set-ExecutionPolicy Bypass -Scope Process -Force
+echo "Installing choclatey..."
+#iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) -Verbose:$false
+echo "Installing VS2019, SSMS, OpenVPN, Office 2016"
+#choco install visualstudio2019professional -y
+#choco install sql-server-management-studio -y
+#choco install openvpn -y
+#choco install microsoft-office-deployment -y
+echo "Resetting Execution Policy to previous value..."
+Set-ExecutionPolicy -Scope Process $execution_policy
+echo "Auto Setup Complete."
