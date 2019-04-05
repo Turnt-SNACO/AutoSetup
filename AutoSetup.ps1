5504bf624a4a2478251512274a36c32d89196f32
James <james@eaiti.com>
2019-04-05 19:47:15 -0400

    Added comments and spacing for readability

--- AutoSetup.ps1
+++ AutoSetup.ps1
@@ -1,14 +1,21 @@
-﻿$Iteration = -1
+﻿#Globals
+$Iteration = -1
 $PercentDone = 0
+
+#Progress Bar
 function DisplayProgress($message)
 {
     $global:Iteration++
     $global:PercentDone = ($global:Iteration / 11 * 100)
     Write-Progress -Activity 'Running AutoSetup' -Status $message -PercentComplete $global:PercentDone
 }
+
+#Execution policy adjust is required to install choclatey
 DisplayProgress("Temporarily adjusting the execution policy for installation")
 $ExecPolicy = (Get-ExecutionPolicy)
 Set-ExecutionPolicy Bypass -Scope Process -Force
+
+#Intstall software
 DisplayProgress("Installing choclatey")
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) -Verbose:$false
 DisplayProgress("Enabling Hyper-V")
@@ -25,15 +32,21 @@ DisplayProgress("Installing Slack")
 choco install slack
 DisplayProgress("Installing Docker")
 choco install docker
+
+#Return execution policy to original state
 DisplayProgress("Resetting Execution Policy to previous value")
 Set-ExecutionPolicy -Scope Process $ExecPolicy
+
+#Clean up globals
 DisplayProgress("Cleaning up")
 Remove-Variable Iteration
 Remove-Variable PercentDone
 Remove-Variable ExecPolicy
 Start-Sleep 1
+
+#Finish and prompt for restart
 Write-Output "Auto Setup Complete."
-$Reboot = Read-Host -Prompt "A reboot is required restart now? (y/n)"
+$Reboot = Read-Host -Prompt "A restart is required. Restart now? (y/n)"
 If ($Reboot -eq "y")
 {
     Restart-Computer
