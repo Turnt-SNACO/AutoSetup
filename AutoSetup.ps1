ceaa6de7adc2cad67aff964e054b933db145f751
James <james@eaiti.com>
2019-04-09 14:45:04 -0400

    removed exec policy code since that needs to be done in order to run script

--- AutoSetup.ps1
+++ AutoSetup.ps1
@@ -6,38 +6,46 @@ $PercentDone = 0
 function DisplayProgress($message)
 {
     $global:Iteration++
-    $global:PercentDone = ($global:Iteration / 12 * 100)
+    $global:PercentDone = ($global:Iteration / 11 * 100)
     Write-Progress -Activity 'Running AutoSetup' -Status $message -PercentComplete $global:PercentDone
 }
 
 #Execution policy adjust is required to install choclatey
-DisplayProgress("Temporarily adjusting the execution policy for installation")
-$ExecPolicy = (Get-ExecutionPolicy)
-Set-ExecutionPolicy Bypass -Scope Process -Force
+#DisplayProgress("Temporarily adjusting the execution policy for installation")
+#$ExecPolicy = (Get-ExecutionPolicy)
+#Set-ExecutionPolicy Bypass -Scope Process -Force
 
 #Intstall software
 DisplayProgress("Installing choclatey")
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) > logfile.txt
+
 DisplayProgress("Enabling Hyper-V")
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All -NoRestart >> logfile.txt
+
 DisplayProgress("Installing Visual Studio 2019 Professional")
 choco install visualstudio2019professional -y >> logfile.txt
+
 DisplayProgress("Installing SQL Server Management Studio")
 choco install sql-server-management-studio -y >> logfile.txt
+
 DisplayProgress("Installing OpenVPN")
 choco install openvpn -y >> logfile.txt
+
 DisplayProgress("Installing Microsoft Office 2016")
 choco install microsoft-office-deployment -y >> logfile.txt
+
 DisplayProgress("Installing Slack")
 choco install slack >> logfile.txt
+
 DisplayProgress("Installing Docker")
 choco install docker >> logfile.txt
+
 DisplayProgress("Installing RoyalTS")
-DisplayProgress("Installing Docker") >> logfile.txt
+choco install royalts >> logfile.txt
 
 #Return execution policy to original state
-DisplayProgress("Resetting Execution Policy to previous value")
-Set-ExecutionPolicy -Scope Process $ExecPolicy
+#DisplayProgress("Resetting Execution Policy to previous value")
+#Set-ExecutionPolicy -Scope Process $ExecPolicy
 
 #Clean up globals
 DisplayProgress("Cleaning up")
